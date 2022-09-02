ba85c66f8c3fdbe66e50a0226eeb3c2f705a96b5
Evgeniy Malygin <black2001@inbox.ru>
2022-09-02 14:35:42 +0300

    Выполнено задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
 public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+    public static boolean checkLeapYear(int year){
+        if (year % 100 == 0 && year % 400 == 0)
+        {
+            return true;
+        } else
+        {
+            if (year % 4 == 0 && year % 100 !=0)
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        // Задание 1
+        System.out.println("========== Задание 1 ==========");
+        int year = 2022;
+        if (checkLeapYear(year)){
+            System.out.println(year + " год високосный");
+        }else
+        {
+            System.out.println(year + " год невисокосный");
+        }
     }
 }
